fddf9fc4798401cd5954f11a311054d4f7a6fec3
quasifu <brian@cheng.ninja>
2022-11-14 18:35:05 -0600

    update cypress tests.

--- frontend/cypress/e2e/1-Browse-Hangry-Hippo/spec.cy.js
+++ frontend/cypress/e2e/1-Browse-Hangry-Hippo/spec.cy.js
@@ -4,12 +4,22 @@ describe('Hits Home Page', () => {
   });
   it('displays the Welcome message', () => {
     // We use the `cy.get()` command to get all elements that match the selector.
-    // Then, we use `should` to assert that there are two matched items,
-    // which are the two default items.
+    // Then, we use `should` to assert that there are is a Welcome message.
     cy.get('h1').should(
       'have.text',
       "Welcome to Hangry Hippo! Hungry? Let's get started!"
     );
+  });
+  it('Shows the Menu items', () => {
+    //Check for the Appeteasers menu item
+    cy.get('[data-testid=category-item]')
+      .should('be.visible')
+      .and('contain', 'Appeteasers');
+
+    //Check for the Handhelds menu item
+    cy.get('[data-testid=category-item]')
+      .should('be.visible')
+      .and('contain', 'Handhelds');
   });
 });
 
@@ -19,8 +29,7 @@ describe('Hits Order Page', () => {
   });
   it('shows Your Order title', () => {
     // We use the `cy.get()` command to get all elements that match the selector.
-    // Then, we use `should` to assert that there are two matched items,
-    // which are the two default items.
+    //Check to see if 'Your Order' exists
     cy.get('h3').should('have.text', 'Your Order');
   });
 });
